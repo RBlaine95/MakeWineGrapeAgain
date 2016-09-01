d571df0a573334b678f18c813e039ee6db873294
mrchocolatezzz <faws1234@gmail.com>
2016-09-01 13:50:33 +0200

    Add files via upload

---
+++ MWGA/src/Start_Main.java
@@ -0,0 +1,13 @@
+
+
+
+public class Start_Main {
+
+    
+    public static void main(String[] args) {
+        
+        Login_UI log = new Login_UI();
+        log.setVisible(true);
+    }
+    
+}
