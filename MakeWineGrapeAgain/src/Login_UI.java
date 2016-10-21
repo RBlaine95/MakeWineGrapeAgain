c1ba22dcb23467bb852b0a3fd7d4c6788e028bcb
mrchocolatezzz <faws1234@gmail.com>
2016-10-21 11:20:01 +0200

    TeamUIBIGchange(not really but ok..)
    Changes lovies
    
    BASIC CHANGES:
    Font changed to Calibri 14 becuze soft curves like a good woman, or Daniele
    Standardized window size, which is 600x600, if smaller it will be within the standard window, if bigger it will display over it
    All UIs will be centered, just how society works kids
    Changed the theme too Nimbus, I would like to thank Goku and the Monkey King, for helping Java name this theme, but come on guys, yous coulda not plagirised
    
    LoginUI
    -Enter keystroke works on button and textfield
    -Changed the size/restructured the UI
    -Added logo to the UI, thanks Darren <3
    -standerdized size, centered, op-top
    
    MainUI
    -Added the escape button keystroke to return to the login menu
    -Added logo to the UI, thanks Darren <3
    -centered, on-top
    
    AdminUI
    -Added background UI image
    -standardized size
    -centered

--- MakeWineGrapeAgain/src/Login_UI.java
+++ MakeWineGrapeAgain/src/Login_UI.java
@@ -1,4 +1,6 @@
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JOptionPane;
 
 /*
@@ -17,6 +19,39 @@ public class Login_UI extends javax.swing.JFrame {
      */
     public Login_UI() {
         initComponents();
+        
+            loginBtn.addKeyListener(new KeyAdapter() {
+    public void keyPressed(KeyEvent e) {
+         if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+              onButtonPress();
+         }
+    } 
+    });
+            
+            passwordField.addKeyListener(new KeyAdapter() {
+    public void keyPressed(KeyEvent e) {
+         if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+              onButtonPress();
+         }
+    } 
+    });
+        
+                try {
+        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            if ("Nimbus".equals(info.getName())) {
+                javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                break;
+            }
+        }
+    } catch (ClassNotFoundException ex) {
+        java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (InstantiationException ex) {
+        java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (IllegalAccessException ex) {
+        java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        java.util.logging.Logger.getLogger(main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+    }
     }
 
     /**
@@ -28,14 +63,18 @@ public class Login_UI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
         loginBtn = new javax.swing.JButton();
         passwordField = new javax.swing.JPasswordField();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setState(0);
+        setPreferredSize(new java.awt.Dimension(600, 600));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagery/MWGA_png_transparent.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 120, -1, -1));
 
         loginBtn.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagery/burntcoaltexturebutton.png"))); // NOI18N
         loginBtn.setBorder(null);
@@ -47,17 +86,18 @@ public class Login_UI extends javax.swing.JFrame {
                 loginBtnActionPerformed(evt);
             }
         });
-        getContentPane().add(loginBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 540, 120, 120));
+        getContentPane().add(loginBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 420, 120, 120));
 
         passwordField.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         passwordField.setToolTipText("Enter your password");
         passwordField.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
+        passwordField.setNextFocusableComponent(loginBtn);
         passwordField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 passwordFieldActionPerformed(evt);
             }
         });
-        getContentPane().add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 560, 390, 80));
+        getContentPane().add(passwordField, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 440, 390, 80));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagery/BarrelWoodFinish.jpg"))); // NOI18N
         jLabel1.setToolTipText("");
@@ -66,13 +106,17 @@ public class Login_UI extends javax.swing.JFrame {
         jLabel1.setFocusable(false);
         jLabel1.setName(""); // NOI18N
         jLabel1.setOpaque(true);
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 610, 600));
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void loginBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginBtnActionPerformed
-       
+        onButtonPress();
+    }//GEN-LAST:event_loginBtnActionPerformed
+    
+    public void onButtonPress(){
         String password = passwordField.getText();
         String ps = "";
         
@@ -88,13 +132,12 @@ public class Login_UI extends javax.swing.JFrame {
             }
         }else{
             JOptionPane.showMessageDialog(null,"Password incorrect");
-        }
-            
-        
-        
-        
-    }//GEN-LAST:event_loginBtnActionPerformed
-
+            passwordField.setText(ps);
+        } 
+    }
+    
+    
+    
     private void passwordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_passwordFieldActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_passwordFieldActionPerformed
@@ -105,6 +148,7 @@ public class Login_UI extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JButton loginBtn;
     private javax.swing.JPasswordField passwordField;
     // End of variables declaration//GEN-END:variables
